fb06aa6895a7d839bef4dd193cde033564eab525
Daniel <daniel.corbellini28@gmail.com>
2023-10-23 11:45:40 -0300

    add wait_for_db command and correcting the formatting of the code

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,9 +2,11 @@
 Calculator functions
 """
 
-def add (x, y):
+
+def add(x, y):
     """Add x and y and return result."""
     return x + y
+
 
 def subtract(x, y):
     """Subtract x from y and return result."""
--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -2,15 +2,14 @@
 Django command to wait for the database to be available.
 """
 import time
-
-from psycopg2 import OperationalError as Psycopg2Error
-
+from psycopg2 import OperationalError as Psycopg2OpError
 from django.db.utils import OperationalError
-from django.core.managment.base import BaseCommand
+from django.core.management.base import BaseCommand
+
 
 class Command(BaseCommand):
     """Django command to wait for database."""
-    
+
     def handle(self, *args, **options):
         """Entrypoint for command."""
         self.stdout.write('Waiting for database...')
@@ -19,8 +18,8 @@ class Command(BaseCommand):
             try:
                 self.check(databases=['default'])
                 db_up = True
-            except (Psycopg2Error, OperationalError):
+            except (Psycopg2OpError, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
-                self.sleep(1)
+                time.sleep(1)
 
         self.stdout.write(self.style.SUCCESS('Database available!'))
