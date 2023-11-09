1dc09a1fed058bf468e97dd68e7c214678e0e54b
Daniel <daniel.corbellini28@gmail.com>
2023-11-09 15:00:19 -0300

    Implement the token and the user API

--- app/user/urls.py
+++ app/user/urls.py
@@ -5,8 +5,9 @@ from django.urls import path
 
 from user import views
 
-app_name = "user"
+app_name = 'user'
 
 urlpatterns = [
-    path('create/', views.CreateUserView.as_view(), name='create')
+    path('create/', views.CreateUserView.as_view(), name='create'),
+    path('token/', views.CreateUserView.as_view(), name='token'),
 ]
