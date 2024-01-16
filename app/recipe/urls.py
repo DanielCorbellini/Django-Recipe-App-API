30cfb813fc0f71e31c31c949bfd135c897e9f62c
unknown <daniel.corbellini28@gmail.com>
2024-01-16 14:13:54 -0300

    Recipe Image API

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -16,5 +16,5 @@ router.register('ingredients', views.IngredientViewSet)
 app_name = 'recipe'
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
 ]
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -4,7 +4,11 @@ Views for the recipe APIs
 from rest_framework import (
     viewsets,
     mixins,
+    status,
 )
+
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
@@ -31,12 +35,26 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Return the serializer class for the request."""
         if self.action == 'list':
             return serializers.RecipeSerializer
+        elif self.action == 'upload_image':
+            return serializers.RecipeImageSerializer
 
         return self.serializer_class
 
     def perform_create(self, serializer):
         """Create a new recipe."""
         serializer.save(user=self.request.user)
+
+    @action(methods=['POST'], detail=True, url_path='upload-image')
+    def upload_image(self, request, pk=None):
+        """Upload an image to recipe."""
+        recipe = self.get_object()
+        serializer = self.get_serializer(recipe, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.erros, status=status.HTTP_400_BAD_REQUEST)
 
 
 class BaseRecipeAttrViewSet(mixins.DestroyModelMixin,
@@ -60,5 +78,5 @@ class TagViewSet(BaseRecipeAttrViewSet):
 
 class IngredientViewSet(BaseRecipeAttrViewSet):
     """Manage ingredients in the database."""
-    serializer_class = serializers.IngredientSerialzier
+    serializer_class = serializers.IngredientSerializer
     queryset = Ingredient.objects.all()
